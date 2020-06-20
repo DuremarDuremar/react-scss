5d6b317ceec496147257ad053322b667f562ccde
DuremarDuremar <duremar98@yandex,ru>
2020-06-20 17:34:32 +0300

    next

---
+++ react-scss/src/components/header/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="header-reg">1</div>
+          <div className="header-form">2</div>
+          <div className="header-nav">3</div>
+          <div className="header-afisha">4</div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
