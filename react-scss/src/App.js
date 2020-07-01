5c05dc57c4b65d475b69d3fc637d4f4cb74d016d
DuremarDuremar <duremar98@yandex,ru>
2020-07-01 15:19:57 +0300

    next

--- react-scss/src/App.js
+++ react-scss/src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
 import "./App.scss";
-import Header from "./components/header/header";
-import HeaderReg from "./components/header/header-reg";
 
 function App() {
-  return (
-    <div className="App">
-      <HeaderReg />
-      <Header />
-      <div className="afisha">Afisha</div>
-      <div className="saidbar">saidbar</div>
-      <div className="content">content</div>
-      <div className="footer">footer</div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
