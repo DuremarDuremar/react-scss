97f4cfc659c3254fafa039aa95d3d01d1b9406fd
DuremarDuremar <duremar98@yandex,ru>
2020-07-05 22:15:09 +0300

    next

---
+++ react-scss/src/components/components_footer/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import User from "./User";
+import Palette from "./Palette";
+
+const Profile = () => {
+  return (
+    <div className="container profile">
+      <User
+        src="https://www.kino-teatr.ru/acter/foto/hollywood/49001.jpg"
+        alt="actor"
+        name="Duglas"
+      />
+      {/* <Palette /> */}
+    </div>
+  );
+};
+
+export default Profile;
--- react-scss/src/components/footer.js
+++ react-scss/src/components/footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import Feed from "../components/components_footer/Feed";
 import "./footer.scss";
 import Header from "./components_footer/Header";
+import Profile from "./components_footer/Profile";
 // import { indexDelete } from "../components/func/indexDelete";
 
 class Footer extends React.Component {
@@ -10,6 +11,7 @@ class Footer extends React.Component {
       <footer>
         <Header />
         <Feed />
+        <Profile />
       </footer>
     );
   }
